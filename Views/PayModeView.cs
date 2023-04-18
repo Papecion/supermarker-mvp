5d69d4e04e0acf88ec241974f3bacd62b79066ca
Pablo Reyes <pabloandresreyes2004@gmail.com>
2023-04-18 11:00:44 -0500

    Pay Mode Presenter

--- Views/PayModeView.cs
+++ Views/PayModeView.cs
@@ -10,11 +10,78 @@ using System.Windows.Forms;
 
 namespace Supermarket_mvp.Views
 {
-    public partial class PayModeView : Form
+    public partial class PayModeView : Form, IPayModeView
     {
+        private bool isEdit;
+        private bool isSuccessful;
+        private string message;
         public PayModeView()
         {
             InitializeComponent();
+            AssociateAndRaiseViewEvents();
+            tabControl1.TabPages.Remove(tabPagePayModeDetail);
+        }
+
+        private void AssociateAndRaiseViewEvents()
+        {
+            BtnSearch.Click += delegate { SearchEvent?.Invoke(this, EventArgs.Empty); };
+
+            TxtSearch.KeyDown += (s, e) =>
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    SearchEvent?.Invoke(this, EventArgs.Empty);
+                }
+            };
+        }
+
+        public string PayModeId
+        {
+            get { return TxtPayModeId.Text; }
+            set { TxtPayModeId.Text = value; }
+        }
+
+        public string PayModeName
+        {
+            get { return TxtPayModeName.Text; }
+            set { TxtPayModeName.Text = value; }
+        }
+        public string PayModeObservation
+        {
+            get { return TxtPayModeObservation.Text; }
+            set { TxtPayModeObservation.Text = value; }
+        }
+        public string SearchValue
+        {
+            get { return TxtSearch.Text; }
+            set { TxtSearch.Text = value; }
+        }
+        public bool IsEdit
+        {
+            get { return isEdit.Text; }
+            set { isEdit.Text = value; }
+        }
+        public bool IsSuccessful
+        {
+            get { return isSuccessful.Text; }
+            set { isSuccessful.Text = value; }
+        }
+        public string Message
+        {
+            get { return message.Text; }
+            set { message.Text = value; }
+        }
+
+        public event EventHandler SearchEvent;
+        public event EventHandler AddNewEvent;
+        public event EventHandler EditEvent;
+        public event EventHandler DeleteEvent;
+        public event EventHandler SaveEvent;
+        public event EventHandler CancelEvent;
+
+        public void SetPayModeListBildingSource(BindingSource payModeList)
+        {
+            DgPayMode.DataSource = payModeList;
         }
 
         private void BtnSearch_Click(object sender, EventArgs e)

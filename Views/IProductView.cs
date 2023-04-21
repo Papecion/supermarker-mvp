68fbc809da431f30ccf9b71234d9c3869a9b07c0
Pablo Reyes <pabloandresreyes2004@gmail.com>
2023-04-21 02:06:59 -0500

    Product

---
+++ Views/IProductView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Views
+{
+    internal interface IProductView
+    {
+        void ShowProducts(List<ProductModel> products);
+        void ShowErrorMessage(string message);
+    }
+}
